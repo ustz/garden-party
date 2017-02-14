7e8b82f15640d378eceec6147fa99d8a8c3e4a11
Ulysse Schwartz <schwartzulysse@gmail.com>
2017-02-14 16:33:40 +0100

    edit user without photo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,16 @@ class UsersController < ApplicationController
   end
 
   def update
+    if current_user.update(user_params)
+      redirect_to gardens_path
+    else
+      render :edit
+    end
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :city, :email)
   end
 end
