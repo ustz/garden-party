56bba2c0f0304bffef69dd6811682ef48de41c81
Aurelien Burget <aurelion@gmail.com>
2017-02-14 11:49:29 +0100

    saving chamge on router and gardens controller

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,2 +1,13 @@
 class GardensController < ApplicationController
+  def index
+    @gardens = Garden.all
+  # @gardens = Garden.where(user_id: current_user)
+  end
+
+  private
+
+  # Strong params
+  def garden_params
+    Params.require(:garden).permit(:title, :description)
+  end
 end
