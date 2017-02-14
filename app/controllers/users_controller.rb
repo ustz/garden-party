5f26634e976535dbd53a02baee68d730752605c7
Ulysse Schwartz <schwartzulysse@gmail.com>
2017-02-14 15:55:02 +0100

    edit user setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+
+  end
 end
