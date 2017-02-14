b0744bf4466be0ecc388251e968c628b33b35b1b
Ulysse Schwartz <schwartzulysse@gmail.com>
2017-02-14 14:26:34 +0100

    navbarv1

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,4 +1,6 @@
 class GardensController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
   def index
     @gardens = Garden.all
   # @gardens = Garden.where(user_id: current_user)
