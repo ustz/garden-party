feea6c2c76ff7c977c95f560e3aed3e1ec81c2e5
Olga Starkova <olstar.ru@gmail.com>
2017-02-16 15:58:47 +0100

    accept/reject feature done + booking total price and length added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :store_current_location, :unless => :devise_controller?    
   before_action :authenticate_user!
+
+  private
+
+  def store_current_location
+  	if params[:controller] == 'bookings' && params[:action] == 'create'
+  		store_location_for(:user, request.url.gsub('bookings', ''))
+  	end
+  end
 end
