ef411ee260d1e55e5d69f37bc2ecc0708d92a745
Ulysse Schwartz <schwartzulysse@gmail.com>
2017-02-17 15:23:04 +0100

    remove mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
 
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
 
 
 def self.find_for_facebook_oauth(auth)
